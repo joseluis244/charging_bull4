1ad760e9e6c67e38fb854ea804768a863e675276
joseluis244 <32981592+joseluis244@users.noreply.github.com>
2018-09-09 21:03:35 -0400

    a

--- server2.js
+++ server2.js
@@ -10,15 +10,16 @@ app.get("/",function(req,res){
     res.sendfile("a.html");
 })
 app.post("/",upload.single("ss"),function(req,res,next){
-    fs.rename(req.file.path,renombre(req.file),function(){})
+    fs.rename(req.file.path,renombre(req.file))
     res.send("asd")
 })
 app.listen(3000,function(){
     console.log("inicio")
 })
 function renombre (a){
-    switch (a.mimetype){
+    /*switch (a.mimetype){
         case "image/jpeg":
             return a.path+".jpg";
-    }
+    }*/
+    return a.path+".jpg";
 }
