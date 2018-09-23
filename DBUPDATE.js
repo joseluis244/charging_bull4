d4beb8900b60121c3af2c0abdd5394946de15adf
joseluis244 <32981592+joseluis244@users.noreply.github.com>
2018-09-23 15:45:04 -0400

    a

--- DBUPDATE.js
+++ DBUPDATE.js
@@ -1,11 +1,11 @@
-
+"use strict";
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1/RB2");
 const clientes =require("./models/clientes");
 
 clientes.find({ $or: [ { "distribuye": true }, { "distribuye": false } ] },function(err,cli){
     let ultimavisita = 0;
-    for(i=0;i<=cli.length-1;i++){
+    for(let i=0;i<=cli.length-1;i++){
         if(cli[i].ultima_visita > 0){
             console.log("existe");
         }
