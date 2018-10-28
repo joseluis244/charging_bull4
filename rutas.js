d7b26a40ed749c43f5f41e6b2a593d5c65eb6c40
joseluis244 <32981592+joseluis244@users.noreply.github.com>
2018-10-28 18:41:48 -0400

    ss

--- rutas.js
+++ rutas.js
@@ -68,7 +68,6 @@ function RGET (app,passport) {
     app.get("/ficha",isLoggedIn,function(req,res){
         var id = req.param("clid");
         clientes.findById(id,function(err,cli){
-            console.log(cli)
             res.render("ficha_cliente.ejs",{cli,cli});
         })
     })
